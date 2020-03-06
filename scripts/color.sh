b9e75ddcd862b476fc00614810f830462500ed93
ABM Ruman <abm.ruman@gmail.com>
2020-03-06 19:20:50 +0600

    Add traefik support

---
+++ scripts/color.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#title        : test.sh
+#description  : shell color codes
+#author       : abmruman
+#usage        : `source ./scripts/test.sh`
+
+# color
+RESET=$'\e[1;0m'
+RED=$'\e[1;31m'
+GREEN=$'\e[1;32m'
+YELLOW=$'\e[1;33m'
+RED_BACK=$'\e[101m'
+GREEN_BACK=$'\e[102m'
+YELLOW_BACK=$'\e[103m'
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,19 +4,26 @@
 #author       : abmruman
 #usage        : `./scripts/test.sh` OR `make test`
 
-set -ev
+set -e
+source ./scripts/color.sh
 
 shopt -s expand_aliases
 alias curl="curl -ILsS -X GET"
-alias grep="grep -A 5 --color=auto"
-alias echo="echo -e"
+alias egrepc="egrep -o --color=auto"
+alias echoe=" echo -e"
 
+[[ -z "$HOST" ]] && eval $(egrep '^HOST' .env | xargs)
 
-eval $(egrep '^HOST' .env | xargs)
+checkmark="$GREEN\xE2\x9C\x94 $RESET"
+crossmark="$RED\xE2\x9D\x8C $RESET"
+regex='HTTP/[12]([\.][0-9])? 200'
 
-curl $HOST | grep '200 OK' || exit 1
+echoe "crul $YELLOW$HOST$RESET: "
+curl $HOST | egrepc "$regex" && echoe "$checkmark PASSED" || echoe "$crossmark FAILED"
 echo
-curl $HOST/v2 | grep '200 OK' || exit 1
+echoe "crul $YELLOW$HOST/v2: "
+curl $HOST/v2 | egrepc "$regex" && echoe "$checkmark PASSED" || echoe $crossmark "FAILED"
 echo
-curl $HOST/v2/_catalog | grep '200 OK' || exit 1
+echoe "crul $YELLOW$HOST/v2/_catalog$RESET: "
+curl $HOST/v2/_catalog | egrepc "$regex" && echoe "$checkmark PASSED" || echoe $crossmark "FAILED"
 echo
