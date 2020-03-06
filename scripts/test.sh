90546cf86ad6901c5413b880994d4f7c67e01f9d
ABM Ruman <abm.ruman@gmail.com>
2020-03-07 00:40:40 +0600

    rewrite the test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 #title        : test.sh
-#description  : tests
+#description  : tests registry HTTP status with curl
 #author       : abmruman
 #usage        : `./scripts/test.sh` OR `make test`
 
 set -e
+
 source ./scripts/color.sh
 
+FAILED=0
+
 shopt -s expand_aliases
-alias curl="curl -ILsS -X GET"
+alias curlf="curl -ILsS -X GET"
 alias egrepc="egrep -o --color=auto"
 alias echoe=" echo -e"
 
+curl_test_200() {
+  checkmark="$GREEN\xE2\x9C\x94 $RESET"
+  crossmark="$RED\xE2\x9D\x8C $RESET"
+  pattern='HTTP/[0-9]([\.][0-9])? 200'
+
+  match=`curlf $1 | egrepc -c "$pattern"`
+
+  if [ "$match" -gt 0 ] ; then
+    echoe "$checkmark PASSED"
+    status=0
+  else
+    echoe "$crossmark FAILED"
+    status=1
+  fi
+  echo
+}
+
+test_uri() {
+  echo
+  uri="$1"
+  echoe "${YELLOW}Testing URI:${RESET} $1"
+
+  curl_test_200 "$uri" || (echo $? >/dev/null)
+  [[ $status -gt 0 ]] && FAILED=1
+  curlf "$uri"
+}
+
+
 [[ -z "$HOST" ]] && eval $(egrep '^HOST' .env | xargs)
 
-checkmark="$GREEN\xE2\x9C\x94 $RESET"
-crossmark="$RED\xE2\x9D\x8C $RESET"
-regex='HTTP/[12]([\.][0-9])? 200'
+test_uri "$HOST"
+test_uri "$HOST/v2"
+test_uri "$HOST/v2/_catalog"
 
-echoe "crul $YELLOW$HOST$RESET: "
-curl $HOST | egrepc "$regex" && echoe "$checkmark PASSED" || echoe "$crossmark FAILED"
-echo
-echoe "crul $YELLOW$HOST/v2: "
-curl $HOST/v2 | egrepc "$regex" && echoe "$checkmark PASSED" || echoe $crossmark "FAILED"
-echo
-echoe "crul $YELLOW$HOST/v2/_catalog$RESET: "
-curl $HOST/v2/_catalog | egrepc "$regex" && echoe "$checkmark PASSED" || echoe $crossmark "FAILED"
-echo
+
+exit $FAILED
